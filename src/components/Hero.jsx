39871ffb2336010a72c8735086b08074955292b4
thunderbolt <dhruvagarwal@jklu.edu.in>
2025-06-25 13:10:01 +0530

    implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,44 +1,70 @@
-import { useGSAP } from "@gsap/react"
-import {SplitText} from 'gsap/all';
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { Timeline } from "gsap/gsap-core";
-gsap.registerPlugin(SplitText);
+import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-    useGSAP(()=>{
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+    useGSAP(() => {
         // This we have written to make the animation split into the each chars and in lines.
-        const heroSplit  = new SplitText('.title',{type:'chars, words'});
-        const paragraphSplit  = new SplitText('.subtitle',{type:'lines'});     
+        const heroSplit = new SplitText('.title', { type: 'chars, words' });
+        const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
 
         //  we have add this line to make the effect of gradient and make it look more 3D
         heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
-        gsap.from(heroSplit.chars,{
-            yPercent:100,
-            duration:1.8,
-            ease:'expo.out',
-            stagger:0.05
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
         });
 
-        gsap.from(paragraphSplit.lines,{
-            opacity:0,
-            yPercent:100,
-            duration:1.8,
-            ease:'expo.out',
-            stagger:0.06,
-            delay:1,
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1,
         });
 
         gsap.timeline({
-            scrollTrigger:{
-                trigger:"#hero",
+            scrollTrigger: {
+                trigger: "#hero",
                 start: 'top top',
                 end: 'bottom top',
                 scrub: true,
             }
         })
-        .to('.right-leaf',{y:200},0)
-        .to('.left-leaf',{y:-200},0)
-    },[]);
+            .to('.right-leaf', { y: 200 }, 0)
+            .to('.left-leaf', { y: -200 }, 0)
+            .to(".arrow", { y: 100 }, 0);
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        //video animation timeLine
+        // create the timeLine 
+        // create the timeline with a default duration 
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+
+            },
+        });
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
+
+    }, []);
 
     return (
         <>
@@ -46,12 +72,14 @@ const Hero = () => {
                 <h1 className="title">
                     MOJITO
                 </h1>
-                <img src="/images/hero-left-leaf.png" alt="left-leaf"
-                    className="left-leaf"
+                <img src="/images/hero-left-leaf.png"
+                 alt="left-leaf"
+                 className="left-leaf"
                 />
 
-                <img src="/images/hero-right-leaf.png" alt="right-leaf"
-                    className="right-leaf"
+                <img src="/images/hero-right-leaf.png" 
+                alt="right-leaf"
+                className="right-leaf"
                 />
                 <div className="body">
                     <div className="content">
@@ -64,8 +92,8 @@ const Hero = () => {
                         <div className="view-cocktails">
                             <p className="subtitle">
                                 Every cocktail on our menu is a blend of premium ingredients,
-				                creative flair, and timeless recipes — designed to delight your
-				                senses.
+                                creative flair, and timeless recipes — designed to delight your
+                                senses.
 
                             </p>
                             <a href="#cocktails"> View Cocktails</a>
@@ -74,8 +102,19 @@ const Hero = () => {
                 </div>
 
             </section>
+            <div className="video absolute inset-0 ">
+                <video
+                    ref={videoRef}
+                    muted
+                    playsInline
+                    preload='auto'
+                    src="/videos/output.mp4"
+                />
+            </div>
         </>
-    )
-}
+    );
+};
 
 export default Hero
+
+
